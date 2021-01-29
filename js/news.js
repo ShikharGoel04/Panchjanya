9d795d6414074bac61c42447343e6c2596ea340b
Shikhar Goel <shikhargoel4195@gmail.com>
2021-01-29 13:59:45 +0530

    Website category view

--- js/news.js
+++ js/news.js
@@ -145,17 +145,24 @@ function newsDisp(){
             for(const j in data['categories'])
             {
                 var disp1;
+                var disp2;
                 console.log(data['categories'][j]['category']);
                 if(data['categories'][j]['category']!=null)
                 {
+                    disp2='<button class="btn btn-lg my-2 button1" id=%id% value=%category1% onclick="category(%idd%)">%category% </button>';
                     disp1='<button class="btn btn-lg my-2 button1" id=%id% value=%category1% onclick="category(%idd%)">%category% </button>';
                     var newhtml5 = disp1.replace('%id%','check'+totalCat);
                     var newhtml5 = newhtml5.replace('%idd%','check'+totalCat);
                     var newhtml5 = newhtml5.replace('%category1%',data['categories'][j]['category']);
                     var newhtml5 = newhtml5.replace('%category%',data['categories'][j]['category']);
+                    var newhtml6 = disp2.replace('%id%','check'+totalCat);
+                    var newhtml6 = newhtml6.replace('%idd%','check'+totalCat);
+                    var newhtml6 = newhtml6.replace('%category1%',data['categories'][j]['category']);
+                    var newhtml6 = newhtml6.replace('%category%',data['categories'][j]['category']);
                     console.log(newhtml5);
                     totalCat++;
                     document.querySelector('.content2').insertAdjacentHTML('beforeend' , newhtml5);
+                    document.querySelector('.content3').insertAdjacentHTML('beforeend' , newhtml5);
                 }
            
          }
